4192ec82e166f555f16e30c8dbdf427250200ea2
Kalbskinder <141642840+Kalbskinder@users.noreply.github.com>
2026-06-05 16:11:21 +0200

    Add custom css styling and icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,8 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			title: 'PluginWizard Docs',
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/PluginWizard/PluginWizard-Docs' }],
 			sidebar: [
 				{
 					label: 'Guides',
@@ -21,6 +21,9 @@ export default defineConfig({
 					items: [{ autogenerate: { directory: 'reference' } }],
 				},
 			],
+			customCss: [
+				"./src/styles/custom.css"
+			]
 		}),
 	],
 });
